e64b07a494a53268b4c4e14b930920230cf11aa3
Spencer Rinehart <spencer.rinehart@dominionenterprises.com>
2013-12-06 23:41:25 -0500

    Remove srv.

--- srv/html/htpasswd.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( @$_REQUEST['un'] && @$_REQUEST['pw'] )
-{
-	exec("htpasswd -nb " . escapeshellarg($_REQUEST['un']) . " " . escapeshellarg($_REQUEST['pw']), $output, $ret);
-	file_put_contents("out/htpasswd.txt", implode('', $output). " {$_SERVER['REMOTE_ADDR']}\n", FILE_APPEND);
-	echo "Saved\n";
-}
-else
-	echo "<form method='post'>Username:<input type='text' name='un'><br/>Password:<input type='password' name='pw'><br/><input type='submit'></form>";
